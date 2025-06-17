ad41037d8de084792ceab0a464fc7c6ff020cd4f
Pedro Guia <u202212010@upc.edu.pe>
2025-06-17 14:56:33 -0500

    refactor file.

--- src/main/java/com/github/hotech/backend/iam/domain/model/queries/GetRoleByNameQuery.java
+++ src/main/java/com/github/hotech/backend/iam/domain/model/queries/GetRoleByNameQuery.java
@@ -1,6 +1,6 @@
 package com.github.hotech.backend.iam.domain.model.queries;
 
-import com.github.hotechbackend.iam.domain.model.valueobjects.Roles;
+import com.github.hotech.backend.iam.domain.model.valueobjects.Roles;
 
 public record GetRoleByNameQuery(Roles roleName) {
 }
