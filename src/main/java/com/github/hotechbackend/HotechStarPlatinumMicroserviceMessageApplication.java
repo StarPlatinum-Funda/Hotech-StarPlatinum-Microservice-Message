ec7efff9a16fccf910a808e1c7828dd63f33d95a
Pedro Guia <u202212010@upc.edu.pe>
2025-06-17 14:19:11 -0500

    chore: change package root.

--- src/main/java/com/github/hotechbackend/HotechStarPlatinumMicroserviceMessageApplication.java
+++ src/main/java/com/github/hotechbackend/HotechStarPlatinumMicroserviceMessageApplication.java
@@ -1,4 +1,4 @@
-package com.github.hotechstarplatinummicroservicemessage;
+package com.github.hotechbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/github/hotechbackend/HotechStarPlatinumMicroserviceMessageApplicationTests.java
+++ src/test/java/com/github/hotechbackend/HotechStarPlatinumMicroserviceMessageApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.hotechstarplatinummicroservicemessage;
+package com.github.hotechbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
