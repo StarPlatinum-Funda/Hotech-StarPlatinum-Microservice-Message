392b9a84d05c2a05c329bdda6d5def1268d0325b
Pedro Guia <u202212010@upc.edu.pe>
2025-06-17 23:30:57 -0500

    feat(message): fix UpdateMessageCommand.

--- src/main/java/com/github/hotech/backend/message/domain/model/commands/UpdateMessageCommand.java
+++ src/main/java/com/github/hotech/backend/message/domain/model/commands/UpdateMessageCommand.java
@@ -6,4 +6,30 @@ public record UpdateMessageCommand(Long id,
                                    String content,
                                    Long notificationId,
                                    Long userAccountId) {
+
+    public UpdateMessageCommand {
+        if (id == null || id <= 0) {
+            throw new IllegalArgumentException("Id must be greater than 0");
+        }
+
+        if (receptor == null || receptor.isBlank()) {
+            throw new IllegalArgumentException("Receptor cannot be null or empty");
+        }
+
+        if (sender == null || sender.isBlank()) {
+            throw new IllegalArgumentException("Sender cannot be null or empty");
+        }
+
+        if (content == null || content.isBlank()) {
+            throw new IllegalArgumentException("Content cannot be null or empty");
+        }
+
+        if (notificationId == null || notificationId <= 0) {
+            throw new IllegalArgumentException("Notification Id must be greater than 0");
+        }
+
+        if (userAccountId == null || userAccountId <= 0) {
+            throw new IllegalArgumentException("User Account Id must be greater than 0");
+        }
+    }
 }
