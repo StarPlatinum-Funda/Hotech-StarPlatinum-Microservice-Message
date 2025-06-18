9aa5928fa0cf5560d58e796b67d06a7de2204108
Pedro Guia <u202212010@upc.edu.pe>
2025-06-17 19:32:13 -0500

    feat(message): add message repository.

---
+++ src/main/java/com/github/hotech/backend/message/infrastructure/persistence/jpa/repositories/MessageRepository.java
@@ -0,0 +1,14 @@
+package com.github.hotech.backend.message.infrastructure.persistence.jpa.repositories;
+
+import com.github.hotech.backend.message.domain.model.aggregates.Message;
+import com.github.hotech.backend.message.domain.model.valueobjects.NotificationId;
+import com.github.hotech.backend.message.domain.model.valueobjects.UserAccountId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> getMessageByNotificationIdAndUserAccountId(NotificationId notificationId, UserAccountId userAccountId);
+}
