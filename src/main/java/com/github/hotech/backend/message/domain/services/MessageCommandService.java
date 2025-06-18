998dc66e407a8c873ce60ffb5bcce4e5e563fc97
Pedro Guia <u202212010@upc.edu.pe>
2025-06-17 23:58:05 -0500

    feat(message): update services.

--- src/main/java/com/github/hotech/backend/message/domain/services/MessageCommandService.java
+++ src/main/java/com/github/hotech/backend/message/domain/services/MessageCommandService.java
@@ -5,9 +5,11 @@ import com.github.hotech.backend.message.domain.model.commands.CreateMessageComm
 import com.github.hotech.backend.message.domain.model.commands.DeleteMessageCommand;
 import com.github.hotech.backend.message.domain.model.commands.MarkMessageAsReadCommand;
 import com.github.hotech.backend.message.domain.model.commands.UpdateMessageCommand;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public interface MessageCommandService {
     Optional<Message> handle(CreateMessageCommand command);
     Optional<Message> handle(UpdateMessageCommand command);
--- src/main/java/com/github/hotech/backend/message/domain/services/MessageQueryService.java
+++ src/main/java/com/github/hotech/backend/message/domain/services/MessageQueryService.java
@@ -3,10 +3,12 @@ package com.github.hotech.backend.message.domain.services;
 import com.github.hotech.backend.message.domain.model.aggregates.Message;
 import com.github.hotech.backend.message.domain.model.queries.GetMessageByIdQuery;
 import com.github.hotech.backend.message.domain.model.queries.GetMessagesByNotificationIdAndUserAccountIdQuery;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public interface MessageQueryService {
     Optional<Message> handle(GetMessageByIdQuery query);
     List<Message> handle(GetMessagesByNotificationIdAndUserAccountIdQuery query);
