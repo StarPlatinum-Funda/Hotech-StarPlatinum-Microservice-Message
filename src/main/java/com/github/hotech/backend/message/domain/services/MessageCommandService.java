3e3885a8997870a0bb207405bb761211a5d67c4c
Pedro Guia <u202212010@upc.edu.pe>
2025-06-17 22:12:25 -0500

    feat(message): fix message command service.

--- src/main/java/com/github/hotech/backend/message/domain/services/MessageCommandService.java
+++ src/main/java/com/github/hotech/backend/message/domain/services/MessageCommandService.java
@@ -2,11 +2,15 @@ package com.github.hotech.backend.message.domain.services;
 
 import com.github.hotech.backend.message.domain.model.aggregates.Message;
 import com.github.hotech.backend.message.domain.model.commands.CreateMessageCommand;
+import com.github.hotech.backend.message.domain.model.commands.DeleteMessageCommand;
 import com.github.hotech.backend.message.domain.model.commands.MarkMessageAsReadCommand;
+import com.github.hotech.backend.message.domain.model.commands.UpdateMessageCommand;
 
 import java.util.Optional;
 
 public interface MessageCommandService {
     Optional<Message> handle(CreateMessageCommand command);
+    Optional<Message> handle(UpdateMessageCommand command);
+    Optional<Message> handle(DeleteMessageCommand command);
     void handle(MarkMessageAsReadCommand command);
 }
