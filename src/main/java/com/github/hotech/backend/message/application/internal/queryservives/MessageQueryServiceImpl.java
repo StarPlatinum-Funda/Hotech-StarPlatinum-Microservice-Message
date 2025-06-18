88c1c5de668d30a1a32e16f2f61be70979b1eae1
Pedro Guia <u202212010@upc.edu.pe>
2025-06-17 23:45:50 -0500

    feat(message): add message query service implementation.

---
+++ src/main/java/com/github/hotech/backend/message/application/internal/queryservives/MessageQueryServiceImpl.java
@@ -0,0 +1,32 @@
+package com.github.hotech.backend.message.application.internal.queryservives;
+
+import com.github.hotech.backend.message.domain.model.aggregates.Message;
+import com.github.hotech.backend.message.domain.model.queries.GetMessageByIdQuery;
+import com.github.hotech.backend.message.domain.model.queries.GetMessagesByNotificationIdAndUserAccountIdQuery;
+import com.github.hotech.backend.message.domain.model.valueobjects.NotificationId;
+import com.github.hotech.backend.message.domain.model.valueobjects.UserAccountId;
+import com.github.hotech.backend.message.domain.services.MessageQueryService;
+import com.github.hotech.backend.message.infrastructure.persistence.jpa.repositories.MessageRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MessageQueryServiceImpl implements MessageQueryService {
+    private final MessageRepository messageRepository;
+
+    public MessageQueryServiceImpl(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    @Override
+    public Optional<Message> handle(GetMessageByIdQuery query) {
+        return messageRepository.findById(query.id());
+    }
+
+    @Override
+    public List<Message> handle(GetMessagesByNotificationIdAndUserAccountIdQuery query) {
+        return messageRepository.getMessageByNotificationIdAndUserAccountId(new NotificationId(query.notificationId()), new UserAccountId(query.userAccountId()));
+    }
+}
